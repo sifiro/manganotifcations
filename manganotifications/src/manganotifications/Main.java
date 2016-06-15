04fde571140aafbf6f9ac22907c2705406946093
Sifiro <Sifiro@192.168.1.135>
2016-06-15 20:32:01 +0200

    THIS WORKS!!!!!!!

--- manganotifications/src/manganotifications/Main.java
+++ manganotifications/src/manganotifications/Main.java
@@ -1,38 +1,106 @@
 package manganotifications;
+import org.htmlcleaner.*;
 
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.io.*;
 import javax.net.ssl.HttpsURLConnection;
-import java.security.*;
+import javax.xml.xpath.*;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import java.util.ArrayList;
 public class Main {
+	static ArrayList <Manga> coll = new ArrayList <Manga>();
     final String template="https://www.mangaupdates.com/series.html?id=";
-    String url;
-    public Main(String ID){
-    url=template.substring(0)+ID.substring(0);
+    public Main(){
     }
 
+    public void go() throws Exception{
+    	for (int i=0;i<coll.size();i++){
+    		System.out.println(get_vol(i));
+    	}	
+    }
+    
+    public int get_vol(int i) throws XPathExpressionException, ParserConfigurationException, SAXException, IOException, XPatherException, Exception{
+    	String a =(xpath(clear(page(template.substring(0)+coll.get(i).getID().substring(0)))));
+    	return Integer.parseInt(a.substring(0, a.indexOf(' ')));
+    }
+    
     public static void main(String[] args)throws Exception {
-        Main manga = new Main("778");
-        manga.page();
+    	// Add Example Manga
+    	coll.add(new Manga("Hayate no Gotoku!","778",0));
+    	coll.add(new Manga("Jojo","162",0));
+    	coll.add(new Manga("INVALID","99999999",0));
+    	new Main().go();
+    	
     }
 	
-    public BufferedReader page() throws Exception {
+    // This Function return a page in format HTML
+    public String page(String url) throws Exception {
     System.setProperty("java.protocol.handler.pkgs","javax.net.ssl.HttpsURLConnection");
-    URL manga = new URL(this.url);
+    URL manga = new URL(url);
         HttpsURLConnection con = (HttpsURLConnection)manga.openConnection();
         InputStreamReader isr = new InputStreamReader(con.getInputStream());
+		StringBuilder sb = new StringBuilder();
         BufferedReader page = new BufferedReader(isr);
-        String inputLine;
-        
-        while((inputLine = page.readLine())!=null){
-                  System.out.println(inputLine);
+        //String inputLine;
+        int cp;
+        while((cp = page.read())!=-1){
+                  sb.append((char)cp);
         }
-    return page;
+    return sb.toString();
     }
     
-    public void xpath(){
-    //Hayate no Gotoku Example:
-    // /html/head/meta/meta/meta/meta/link/link/link/link/link/body/div/table/tbody/tr/tr/td/table/tbody/tr/td/td/tr/td/table/tbody/tr/td/table/tbody/tr/tr/td/form/input/table/tbody/tr/td/img/br/br/br/br/br/td/table/tbody/tr/td/div/tr/td/table/tbody/tr/td/div/div/div/div/br/br/div/br/br/div/br/br/br/br/br/div/br/br/br/br/div/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/br/div/br/br/br/div
+	public static String page3(String myURL) {
+		System.out.println("crunchifyGetURLContents() is hitting : " + myURL);
+		StringBuilder sb = new StringBuilder();
+		HttpsURLConnection urlConn = null;
+		InputStreamReader in = null;
+		try {
+			URL url = new URL(myURL);
+			urlConn = (HttpsURLConnection)url.openConnection();
+			if (urlConn != null)
+				urlConn.setReadTimeout(60 * 1000);
+			if (urlConn != null && urlConn.getInputStream() != null) {
+				in = new InputStreamReader(urlConn.getInputStream(), Charset.defaultCharset());
+				BufferedReader bufferedReader = new BufferedReader(in);
+				if (bufferedReader != null) {
+					int cp;
+					while ((cp = bufferedReader.read()) != -1) {
+						sb.append((char) cp);
+					}
+					bufferedReader.close();
+				}
+			}
+			in.close();
+		} catch (Exception e) {
+			throw new RuntimeException("Exception while calling URL:" + myURL, e);
+		}
+		
+		return sb.toString();
+	}
     
+	public Document clear(String page) throws XPatherException, ParserConfigurationException{
+		CleanerProperties props = new CleanerProperties();
+		props.setOmitComments(true);
+		HtmlCleaner cleaner = new HtmlCleaner();
+    	TagNode la = cleaner.clean(page);
+    	return new DomSerializer(new CleanerProperties()).createDOM(la);
+	}
+	
+    public String xpath(Document document) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException{
+    //Hayate no Gotoku Example:
+    	String cod="//*[@class='sContent'][7]/text()";
+    	XPathFactory xPathfactory = XPathFactory.newInstance();
+		XPath xpath = xPathfactory.newXPath();
+		XPathExpression expr = xpath.compile(cod);
+        return expr.evaluate(document, XPathConstants.STRING).toString();
     }
 }
---
+++ manganotifications/src/manganotifications/Manga.java
@@ -0,0 +1,31 @@
+package manganotifications;
+
+public class Manga {
+private String name;
+private String ID;
+
+private int vol;
+public Manga(String name,String ID,int vol){
+	this.setID(ID);
+	this.setName(name);
+	this.setVol(vol);
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getID() {
+	return ID;
+}
+public void setID(String iD) {
+	ID = iD;
+}
+public int getVol() {
+	return vol;
+}
+public void setVol(int vol) {
+	this.vol = vol;
+}
+}
